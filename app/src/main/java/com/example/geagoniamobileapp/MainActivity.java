a12e1fa1f025f9f38b5a1820145ffbdfcc73edd6
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2023-02-15 16:40:07 +0800

    Add clicker helloworld and transfer other buttons to helloworld menu

--- app/src/main/java/com/example/geagoniamobileapp/MainActivity.java
+++ app/src/main/java/com/example/geagoniamobileapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.geagoniamobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button6);
+        button.setOnClickListener(view -> openHelloWorldMenu());
+    }
+    public void openHelloWorldMenu(){
+        Intent intent = new Intent(this, HelloworldMenu.class);
+        startActivity(intent);
     }
 }
