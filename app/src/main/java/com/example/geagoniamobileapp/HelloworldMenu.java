1616792475f19b55028f8e5caa4365b93fb836e5
Mark Yuri Geagonia <markyuri.geagonia@cit.edu>
2023-02-15 16:20:52 +0800

    Add helloworldmenu activity

---
+++ app/src/main/java/com/example/geagoniamobileapp/HelloworldMenu.java
@@ -0,0 +1,14 @@
+package com.example.geagoniamobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelloworldMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helloworld_menu);
+    }
+}
